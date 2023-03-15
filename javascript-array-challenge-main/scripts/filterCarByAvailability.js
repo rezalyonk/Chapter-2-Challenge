a08803278e925354c77249e645fff12bbb957d88
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-03-15 23:07:25 +0700

    definisi tipe car

--- javascript-array-challenge-main/scripts/filterCarByAvailability.js
+++ javascript-array-challenge-main/scripts/filterCarByAvailability.js
@@ -1,4 +1,21 @@
 function filterCarByAvailability(cars) {
+  // Definisi tipe cars
+  type Car = {
+    id: string;
+    plate: string;
+    transmission: string;
+    manufacture: string;
+    model: string;
+    available: boolean;
+    type: string;
+    year: number;
+    options: string[];
+    specs: string[];
+  };
+  
+  type Cars = Array<Car>;  
+
+
   // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
 
@@ -11,7 +28,7 @@ function filterCarByAvailability(cars) {
       result.push(car);
     }
   }
-  
+
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
 }
