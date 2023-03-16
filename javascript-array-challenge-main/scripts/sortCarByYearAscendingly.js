8e4ed07e71f9a09552396099abf7ebe3ad188ca6
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-03-16 22:46:01 +0700

    ada perubahan pada code

--- javascript-array-challenge-main/scripts/sortCarByYearAscendingly.js
+++ javascript-array-challenge-main/scripts/sortCarByYearAscendingly.js
@@ -7,6 +7,18 @@ function sortCarByYearAscendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
+  // Fungsi komparator untuk sorting secara ascending berdasarkan tahun (year)
+  function compare(a, b) {
+    if (a.year < b.year) {
+      return -1;
+    }
+    if (a.year > b.year) {
+      return 1;
+    }
+    return 0;
+  }
+  // Lakukan sorting dengan method sort() pada array dan gunakan fungsi komparator compare()
+  result.sort(compare);
 
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;
