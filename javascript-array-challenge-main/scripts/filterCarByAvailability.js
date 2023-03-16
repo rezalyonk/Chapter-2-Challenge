99e10ad563b05a07137d9a33feb6bbaf75edbd3a
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-03-16 19:34:56 +0700

    perubahan code car available

--- javascript-array-challenge-main/scripts/filterCarByAvailability.js
+++ javascript-array-challenge-main/scripts/filterCarByAvailability.js
@@ -6,13 +6,6 @@ function filterCarByAvailability(cars) {
   const result = [];
 
   // Tulis code-mu disini
-  for (const car of cars) {
-    // Jika mobil tersedia, tambahkan ke dalam hasil
-    if (car.available) {
-      result.push(car);
-    }
-  }
-  console.log (result);
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
